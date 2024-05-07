cc252b4c37f25773c21d50530a039b17eec77a76
Sergey <segas95@yandex.ru>
2024-05-07 21:10:00 +0300

    builded

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <div></div>
       <h1>Vite + React</h1>
       <div className="card">
+        <h1>Test</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
